9c6a0ff2d2c18c562132cad50a3cb6a0f4d64be7
Caleb Anderson <2682726+calebd-anderson@users.noreply.github.com>
2025-06-18 08:04:26 -0500

    rename elements data file

--- src/main/java/chemlab/constants/FileConstants.java
+++ src/main/java/chemlab/constants/FileConstants.java
@@ -2,7 +2,7 @@ package chemlab.constants;
 
 public class FileConstants {
     public static final String USER_IMAGE_PATH = "/user/image/";
-    public static final String PERIODIC_TABLE_PATH = "static/data/new_elms.json";
+    public static final String PERIODIC_TABLE_PATH = "static/data/all_elements.json";
     public static final String JPG_EXTENSION = "jpg";
     public static final String USER_FOLDER = System.getProperty("user.home") + "/team2/user/";
     public static final String DIRECTORY_CREATED = "Created directory for: ";
