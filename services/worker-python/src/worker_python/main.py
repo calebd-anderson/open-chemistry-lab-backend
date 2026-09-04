2dbf7e79bd0ebeec59c36854334f9b54329595f1
Caleb Anderson <2682726+calebd-anderson@users.noreply.github.com>
2026-09-04 16:14:16 -0500

    configure vscode python for rdkit

--- services/worker-python/src/worker_python/main.py
+++ services/worker-python/src/worker_python/main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
-from rdkit import Chem
+from rdkit import Chem, DataStructs
+from rdkit.Chem import AllChem
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    m = Chem.MolFromSmiles('Cc1ccccc1')
-    return {"message": "Hello World", "smiles": Chem.MolToSmiles(m)}
+    ms = [Chem.MolFromSmiles('CCOC'), Chem.MolFromSmiles('CCO'), Chem.MolFromSmiles('COC')]
+    fpgen = AllChem.GetRDKitFPGenerator() # noqa
+    fps = [fpgen.GetFingerprint(x) for x in ms]
+    # DataStructs.TanimotoSimilarity(fps[0],fps[1])
+    # DataStructs.TanimotoSimilarity(fps[0],fps[2])
+    # DataStructs.TanimotoSimilarity(fps[1],fps[2])
+    return {"message": "Hello World", "smiles": DataStructs.TanimotoSimilarity(fps[1],fps[2])}
