d190ec2852a07d50014f7b1497e001feec61c3ae
Caleb Anderson <2682726+calebd-anderson@users.noreply.github.com>
2026-09-06 11:51:53 -0500

    add data data_transform test

--- services/worker-python/tests/test_main.py
+++ services/worker-python/tests/test_main.py
@@ -4,8 +4,30 @@ from worker_python.main import app
 
 client = TestClient(app)
 
+JSON_DATA = {
+    "PropertyTable": {
+        "Properties": [
+            {
+                "CID": 977,
+                "MolecularFormula": "O2",
+                "MolecularWeight": "31.999",
+                "ConnectivitySMILES": "O=O",
+                "InChIKey": "MYMOFIZGZYHOMD-UHFFFAOYSA-N",
+                "Charge": 0,
+                "Fingerprint2D": "AAADcQAAMAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA==",
+                "Title": "Oxygen"
+            }
+        ]
+    }
+}
 
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
+
+
+def test_transform():
+    response = client.post("/transform/", json=JSON_DATA)
+    assert response.status_code == 200
+    assert response.json() == JSON_DATA
