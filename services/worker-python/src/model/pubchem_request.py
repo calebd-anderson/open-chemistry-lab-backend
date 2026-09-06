30f080647ab153b8f3801e4d5fc552e32bb66f8d
Caleb Anderson <2682726+calebd-anderson@users.noreply.github.com>
2026-09-06 16:47:33 -0500

    add multiple cid to model test

--- services/worker-python/src/model/pubchem_request.py
+++ services/worker-python/src/model/pubchem_request.py
@@ -1,43 +1,6 @@
 from pydantic import BaseModel, ConfigDict, Field
 from pydantic.alias_generators import to_pascal
 
-JSON_DATA = ''' {
-    "PropertyTable": {
-        "Properties": [
-            {
-                "CID": 977,
-                "MolecularFormula": "O2",
-                "MolecularWeight": "31.999",
-                "ConnectivitySMILES": "O=O",
-                "InChIKey": "MYMOFIZGZYHOMD-UHFFFAOYSA-N",
-                "Charge": 0,
-                "Fingerprint2D": "AAADcQAAMAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA==",
-                "Title": "Oxygen"
-            },
-            {
-                "CID": 5359597,
-                "MolecularFormula": "O2-",
-                "MolecularWeight": "31.999",
-                "ConnectivitySMILES": "[O-][O]",
-                "InChIKey": "OUUQCZGPVNCOIJ-UHFFFAOYSA-M",
-                "Charge": -1,
-                "Fingerprint2D": "AAADcQAAMAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA==",
-                "Title": "Superoxide"
-            },
-            {
-                "CID": 10313042,
-                "MolecularFormula": "O2",
-                "MolecularWeight": "35.99831922",
-                "ConnectivitySMILES": "O=O",
-                "InChIKey": "MYMOFIZGZYHOMD-XPULMUKRSA-N",
-                "Charge": 0,
-                "Fingerprint2D": "AAADcQAAMAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA==",
-                "Title": "Oxygen-18O2"
-            }
-        ]
-    }
-}'''
-
 class PubChemFastformulaPropertiesResponse(BaseModel):
     model_config = ConfigDict(
         alias_generator=to_pascal,
@@ -67,5 +30,3 @@ class PubChemFastformulaCidProperties(BaseModel):
     charge: int
     fingerprint_2d: str
     title: str
-
-# print(PubChemFastformulaPropertiesResponse.model_validate_json(JSON_DATA))
